cbb0191f97a7cd07f9dcc46b4a79168b1e6193bb
sawfiz <dashu.up@gmail.com>
2023-11-08 09:05:28 +0800

    Add serving html pages

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // import http from 'http'
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.end('Hello World!');
+
+  // req.url has value such as '/about'
+  const pathname = req.url;
+
+  let filename;
+  if (pathname === '/') {
+    filename = 'index.html';
+  } else {
+    // the filename of the html to serve should be, e.g., about.html
+    filename = pathname.slice(1) + '.html';
+  }
+  const filePath = path.join(__dirname, 'public', filename);
+
+  // Read the HTML file and serve
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('404');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
